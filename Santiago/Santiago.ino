532637cb4f94047704bb0e6ab2420daca924c813
Aarontpz <scholaraaron@yahoo.com>
2015-02-07 10:56:04 -0800

    Added Andrew's Changes
    Added Andrew's Santiago.ino code. Normally you'd have more to say than that.

--- Santiago/Santiago.ino
+++ Santiago/Santiago.ino
@@ -4,98 +4,93 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
-
 QuadMotorShields md;
+
 StaticJsonBuffer<200> jsonBuffer;
 StaticJsonBuffer<200> clearJson;
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network.
-// gateway and subnet are optional:
+
+// To obtain server ip: Go to network settings->properties->IpV4 
+// Make sure arduino's ip matches for first three numbers or else
+// connections will fail.
+
+byte server[] = {211, 255, 132, 121};
+byte ip[] = {211, 255, 132, 49};
 
 byte mac[] = {0x90, 0xA2, 0xDA, 0x0E, 0x40, 0x9F};
-
-//byte ip[] = {169, 254, 114, 49};
-//byte server[] = {169, 254, 114, 221};
-//Go to network settings->properties->IpV4-> obtain automatically IP 
-// to reset this, otherwise use the address below
-byte ip[] = {211, 255, 132, 49};
-byte server[] = {211, 255, 132, 121};
 int port = 1900;
-String s = " ";
-String sLast = " ";
 EthernetClient client;
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial) {
-  }
+   Serial.begin(9600);
 
-  Ethernet.begin(mac, ip);
-  //Ethernet.begin(mac);
-  delay(1000);
-  Serial.println("connecting...");
+   Ethernet.begin(mac, ip);
+   delay(1000);
 
-  // if you get a connection, report back via serial:
-  if (client.connect(server, port)) {
-    Serial.println("connected.");
-    //print text to the server
-    client.println("This is a request from the client.");
-  } 
-  else {
-    if (client.connect(server, port)) {
+   Serial.println("connecting...");
+
+   // if you get a connection, report back via serial:
+   if (client.connect(server, port)) {
       Serial.println("connected.");
-      //print text to the server
-      client.println("This is a request from the client.");
-  }
-    else { 
-      Serial.println("connection failed");
-  }
- }
+      client.println("This is a request from the client."); //print to server
+   }
+   else {
+      if (client.connect(server, port)) {
+         Serial.println("connected.");
+         client.println("This is a request from the client.");
+      }
+      else {
+          Serial.println("connection failed");
+      }
+   }
 }
-int x,y,r,z = 0;
+
+/* loop()
+ * Reads message from client char by char and saves to a string.
+ * This string is parsed into a json object using ArduinoJson.
+ * The values for various variables is then obtained from this object. 
+ * Using these various variables we control the robot! */
+
 void loop()
 {
-  // if there are incoming bytes available 
-  // from the server, read them and print them:
-  if (client.available()) {
-    char c = client.read();
-    if(c == ' ')
-    {
-        const int maxCharCount = 100;
-        sLast = s;
-        s = " ";
-        char json[maxCharCount];
-        for(int i=0; i<maxCharCount; i++)
-        {
-          json[i] = sLast[i];
-        }
-        JsonObject& root = jsonBuffer.parseObject(json);
-        x = root["X"];
-        y = root["Y"];
-        r = root["R"];
-        z = root["Z"];
-        
-        Serial.println(sLast);
-        Serial.print("X:");
-        Serial.println(x);
-        Serial.print("Y:");
-        Serial.println(y);
-        Serial.print("Z:");
-        Serial.println(z);
-        Serial.print("R:");
-        Serial.println(r);
-        jsonBuffer = clearJson;
-    }
-    else
-    {
-      s += String(c);
-    }
-    //if(sLast)
-    //jsonString = Json decode sLast
-    //Set motorx to jsonString.x
-    char sensor = 'a';
-    client.println(sensor);
-  }
-  
+   int x,y,r,z = 0;   
+   if (client.available()) {
+      string s = " ";
+      char c = client.read();
+      if(c == ' ') {
+	 int maxCommandLength = 100;
+         char[maxCommandLength] command;
+         s.toCharArray(command, maxCommandLength);
+             
+         JsonObject& root = jsonBuffer.parseObject(command);
+
+	 x = root["X"];
+         y = root["Y"];
+         r = root["R"];
+         z = root["Z"];
+         
+         printDebugInfo(x, y, z, r, s); 
+	 
+         jsonBuffer = clearJson;
+         s = " ";
+      }
+      else {
+         s += String(c);
+      }
+
+      char sensor = 'a';
+      client.println(sensor);
+   }
 }
 
+void printDebugInfo(int x, int y, int z, int r, String s) {    
+   Serial.println(s);
+   
+   Serial.print("X:");
+   Serial.println(x);
+   Serial.print("Y:");
+   Serial.println(y);
+   Serial.print("Z:");
+   Serial.println(z);
+   Serial.print("R:");
+   Serial.println(r);
+}
