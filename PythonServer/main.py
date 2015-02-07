f368e3693cb658430c812a17b15057da081072fa
Aarontpz <scholaraaron@yahoo.com>
2015-02-07 10:31:38 -0800

    Added the actual, updated Python code.
    Updated the Python code, with the actual communications code that we had
    developed last week.

--- PythonServer/main.py
+++ PythonServer/main.py
@@ -26,6 +26,13 @@ def isConnected():
     return j.is_connected(0)
     
 
+def process2DAxis(l, h):
+    """Processes the lateral and horizontal axis of a joystick, to be used as inputs
+    for a motor controller."""
+    Ellipsis
+    
+    
+
 def encapsulateData(fp = "write.txt", **kwargs):
     """Encapsulates all kwargs in their key:value pairs, returns a JSON object.
     Objects to be encapsulated: ALL OF THEM (Axis X-V, Buttons 1-10."""
@@ -33,64 +40,71 @@ def encapsulateData(fp = "write.txt", **kwargs):
     json.dump(kwargs, fp, indent = 4, sort_keys = True)
     
     
-    
-BUFSIZE = 4096    #reasonably sized buffer for data
-##---Socket/TCP-IP Code occurs here ---**
-def send(data = str):
-    conn.send(data.encode())
-def recieve():
-    recieve.data = conn.recv(BUFSIZE)
-    recieve.data = recieve.data.decode()
-    return recieve.data
+#    
+#BUFSIZE = 4096    #reasonably sized buffer for data
+###---Socket/TCP-IP Code occurs here ---**
+#def send(data = str):
+#    conn.send(data.encode())
+#def recieve():
+#    recieve.data = conn.recv(BUFSIZE)
+#    recieve.data = recieve.data.decode()
+#    return recieve.data
 
 ##---Socket/TCP-IP Code ends here ---**
 
     
 if __name__ == "__main__":
-# Create a TCP/IP socket
-#    host = ''
-#    port = 1700
-#    address = (host, port)
-#    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#    server.bind((address))
-#    server.listen(1)
-#    print "Looking for Client to talk to...."
-#    conn, address = server.accept()
-#    output = conn.recv(2048)
-#    print "Message received from client:"
-#    print output
-#    
-#    conn.send("This is a response from the server.")
-#    
+    host = socket.gethostbyname(socket.gethostname())
+    print "HOST: ", host
+    port = 1900
+    address = (host, port)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((address))
+    server.listen(1)
+    print "Looking for Client to talk to...."
+    conn, address = server.accept()
+    output = conn.recv(2048)
+    print "Message received from client:"
+    print output
+
+
 
     #TODO: Realize that this code immediately raises an error if there is no server to connect to
     while True:
-        j.update()
-        y = readAxis('Y')
-        x = readAxis("X")
-        z = readAxis('Z')
-        u = readAxis('U')
-        r = readAxis('R')
-        v = readAxis('Z')
-        b1 = readButton(1)
-        b2 = readButton(2)
-        b3 = readButton(3)
-        b4 = readButton(4)
-        b5 = readButton(5)
-        b6 = readButton(6)
-        b7 = readButton(7)
-        b8 = readButton(8)
-        b9 = readButton(9)
-        b10 = readButton(10)
-        print '''
-        y: %s
-        x: %s
-        z: %s
-        u: %s
-        r: %s
-        v: %s
-        '''%(y, x, z, u, r, v)
-        time.sleep(2)
+        try:
+            j.update()
+            y = readAxis('Y') * 4
+            x = readAxis("X") * 4
+            z = readAxis('Z') * 4
+            u = readAxis('U') * 4
+            r = readAxis('R') * 4
+            v = readAxis('Z') * 4
+            b1 = readButton(1)
+            b2 = readButton(2)
+            b3 = readButton(3)
+            b4 = readButton(4)
+            b5 = readButton(5)
+            b6 = readButton(6)
+            b7 = readButton(7)
+            b8 = readButton(8)
+            b9 = readButton(9)
+            b10 = readButton(10)
+        
+            conn.send("{\"X\":%d,\"Y\":%d,\"Z\":%d,\"R\":%d} "%(x, y, z, r))
+#            print '''
+#            y: %s
+#            x: %s
+#            z: %s
+#            u: %s
+#            r: %s
+#            v: %s
+#            '''%(y, x, z, u, r, v)
+            time.sleep(0.1)
+        except socket.error as error:
+            print "ERROR CAUGHT."
+            server.listen(1)
+            print "Looking for Client to talk to...."
+            conn, address = server.accept()
         
         
         
